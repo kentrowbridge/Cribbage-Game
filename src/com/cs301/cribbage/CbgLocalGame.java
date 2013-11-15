e54487a30035889e84ac94cae42427fd1ad69656
Kenny <trowbrid16@up.edu>
2013-11-14 20:07:10 -0800

    v.11.14

--- src/com/cs301/cribbage/CbgLocalGame.java
+++ src/com/cs301/cribbage/CbgLocalGame.java
@@ -1,14 +1,21 @@
 package com.cs301.cribbage;
 import edu.up.cs301.card.*;
+import edu.up.cs301.game.*;
+import edu.up.cs301.game.actionMsg.GameAction;
 
 /* 
 
  */
 
 
-class CbgLocalGame {
+class CbgLocalGame extends LocalGame{
+	
 	private Deck deck;
 	private CbgState state;
+	private int currentGameStage;
+	
+	private int originalTally;
+	
 	public CbgLocalGame(){
 		deck = new Deck();//creates a deck and shuffles it.
 		deck.add52().shuffle();
@@ -17,59 +24,69 @@ class CbgLocalGame {
 
 	}
 
+	private void gameCycle()
+	{
+		while (!state.getGameOver())
+		{
+			deal();
+			currentGameStage = state.getGameStage();//TODO write getgameStage Method
+			while (currentGameStage == state.THROW_STAGE)
+			{
+				
+				state.setGameStage(state.PEG_STAGE);//TODO make if statement that determines if it is the correct time to switch state
+			}
+			while (currentGameStage == state.PEG_STAGE)
+			{
+				//once the game is in the pegging stage, flips the top card of deck
+				state.getBonusCard();
+				state.setGameStage(state.COUNT_STAGE);
+			}
+			while (currentGameStage == state.COUNT_STAGE)
+			{
+				//after the score has been tallied, checks if the game is over
+				state.getGameOver();
+				originalTally = state.getTally();
+				if (state.getTally() > originalTally)
+				{
+					state.setGameStage(state.THROW_STAGE);
+				}
+			}
+			deck.shuffle();
+		}
+	}//gameCycle
 
 	/*
 	 * Checks the boolean isGameOver in gameState, and if its true it sets the return string to reflect the final score and the winner.
 	 */
 
-	private final String checkIfGameOver() {
-		if(state.getGameOver()){
-			int winner = state.getWinner();
-			if(winner == CbgState.PLAYER_1){
-				return ("Playre 1 won with " + state.getScore(CbgState.PLAYER_1) + " points.");
-			}
-			else if(winner == CbgState.PLAYER_2){
-				return ("Playre 2 won with " + state.getScore(CbgState.PLAYER_1) + " points.");
-			}
-			else return "ERROR";//if something horrible happens
-		}
-
-		return "FALSE";//if game is not over
-
-	}
-
 	private final boolean isValidCard() {
 		boolean isValid = false;
-
+		// TODO
 		return isValid;
 	}
 
 	private final int handCount() {
 		int count = 0;
-
+		// TODO
 		return count;
-	}
-
-	private final void sendUpdatedStateTo() {
-
 	}
 
 	private final boolean canMove() {
 		boolean canMove = false;
-
+		// TODO
 		return canMove;
 
 	}
 
 	private final boolean makeMove() {
 		boolean makeMove = false;
-
+		// TODO
 		return makeMove;
 	}
 
 	private final int countTable(Card table[]) {
 		int count = 0;
-
+		// TODO
 		return count;
 	}
 
@@ -88,5 +105,37 @@ class CbgLocalGame {
 		state.setHand(p2Hand, CbgState.PLAYER_2);
 	}
 
+	@Override
+	protected void sendUpdatedStateTo(GamePlayer p) {
+		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	protected boolean canMove(int playerIdx) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected String checkIfGameOver() {
+		if(state.getGameOver()){
+			int winner = state.getWinner();
+			if(winner == CbgState.PLAYER_1){
+				return ("Player 1 won with " + state.getScore(CbgState.PLAYER_1) + " points.");
+			}
+			else if(winner == CbgState.PLAYER_2){
+				return ("Player 2 won with " + state.getScore(CbgState.PLAYER_1) + " points.");
+			}
+			else return "ERROR";//if something horrible happens
+		}
+
+		return "FALSE";//if game is not over
+	}
+
+	@Override
+	protected boolean makeMove(GameAction action) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
--- src/com/cs301/cribbage/CbgState.java
+++ src/com/cs301/cribbage/CbgState.java
@@ -2,14 +2,23 @@ package com.cs301.cribbage;
 import android.util.Log;
 import edu.up.cs301.card.*;
 
-/* 
-   
+/**
+ * Cribbage State
+ * Version 11/14/2013
+ * @author Nick Sohm
+ * @author Kenny Trowbridge
+ * 
+ *   This class holds the information for the current state of the game. 
  */
 
 
 class CbgState {
+	public final int THROW_STAGE = 1;
+	public final int PEG_STAGE = 2;
+	public final int COUNT_STAGE = 3;
 	public static final int PLAYER_1 = 1;//player constants
 	public static final int PLAYER_2 = 2;
+	
     private Card[] player1Hand;
     private Card[] player2Hand;
     private Card[] crib;
@@ -21,6 +30,7 @@ class CbgState {
     protected int player2Score;
     protected int tally;
     protected Card[] tableArray;
+    
 
     /*
      * Set after score bar is updated, if score exceeds max points this is set to true.
@@ -30,6 +40,20 @@ class CbgState {
     public int MAX_TALLY = 31;
     public int MAX_SCORE = 121;
 
+    public CbgState()
+    {
+        /*crib = null;
+        deck.setDeck();
+        bonusCard = null;
+        gameStage = cbgStage;
+        winner = ;
+        player1Score = ;
+        player2Score = ;
+        tally = 
+        tableArray = ;*/
+
+    }//constructor
+    
     public final void setScore(int score, int player) {
     	if (player == PLAYER_1){
     		player1Score = score;
@@ -41,12 +65,12 @@ class CbgState {
     		Log.i("Player error", "Error, there is no player " + player);
     	}
     	isGameOver();//checks if game is over when score is changed
-    }
+    }//setScore
 
     public final void setTally(int tally) {
     	this.tally = tally;
     
-    }
+    }//setTally
 
     public final void setHand(Card[] hand, int player) {
     	if (player == PLAYER_1){
@@ -58,34 +82,35 @@ class CbgState {
     	else {
     		Log.i("Player error", "Error, there is no player " + player);
     	}
-    }
+    }//setHand
 
     public final void setCrib(Card[] crib) {
     	this.crib = crib;
-    }
+    }//setCrib
 
-    public final void setDeck(Deck deck) {
+    public void setDeck(Deck deck) {
     	this.deck = deck;
-    }
+    }//setDeck
 
     public final void setTable(Card[] table) {
     	this.tableArray = table;
-    }
+    }//setTable
 
     public final void setBonusCard(Card card) {
-    bonusCard = card;
-    }
+    	bonusCard = card;
+    }//setBonusCard
 
     public final void setGameStage(int stage) {
+    	//TODO
     	gameStage = stage;
-    }
+    }//setGameStage
     
     public final void setGameOver(boolean isGameOver){
     	this.isGameOver = isGameOver;
-    }
+    }//setGameOver
     public final void setWinner(int winner){
     	this.winner = winner;
-    }
+    }//setWinnder
 
     public final int getScore(int player) {
     	if(player == PLAYER_1) {
@@ -96,22 +121,25 @@ class CbgState {
     	}
     	else return -1; //for error
     
-    }
+    }//getScore
 
     public final int getTally() {
+    	//TODO
     	return tally;
     
-    }
+    }//getTally
 
     public final Card[] getCrib() {
+    	//TODO
     	return crib;
     
-    }
+    }//getCrib
 
     public final Card[] getTable() {
+    	//TODO
     	return tableArray;
     
-    }
+    }//getTable
 
     public final Card[] getHand(int player) {
     	if (player == PLAYER_1){
@@ -121,24 +149,44 @@ class CbgState {
     		return player2Hand;
     	}
     	else return null;// if error
-    }
+    }//getHand
 
     public final Deck getDeck() {
+    	//TODO
     	return deck;
-    }
+    }//getDeck
 
     public final Card getBonusCard() {
+    	//TODO
     	return bonusCard;
-    }
+    }//getBonusCard
 
     public final boolean getGameOver() {
+    	//TODO    	
     	return isGameOver;
     
-    }
+    }//getGameOver
 
     public final int getGameStage() {
+    	int p1Counter = 0;
+    	int p2Counter = 0;
+    	for(int i = 0; i<player1Hand.length; i++){
+    		if(player1Hand[i] != null){
+    			p1Counter++;
+    		}
+    		if(player2Hand[i] != null){
+    			p2Counter++;
+    		}
+    	}
+    	if(p1Counter == 6 && p2Counter == 6){
+    		return THROW_STAGE;
+    	}else if(p1Counter <= 4 && p2Counter <= 4) {
+    		return PEG_STAGE;
+    	}else if (p1Counter == 0 && p2Counter ==0){
+    		return COUNT_STAGE;
+    	}
     	return gameStage;
-    }
+    }//getGameStage
    
     public void isGameOver(){
     	if(player1Score >= MAX_SCORE){
@@ -149,10 +197,8 @@ class CbgState {
     		setGameOver(true);
     		setWinner(PLAYER_2);
     	}
-    }
+    }//isGameOver
     public int getWinner(){
     	return winner;
-    }
-
-
+    }//getWinner
 }
