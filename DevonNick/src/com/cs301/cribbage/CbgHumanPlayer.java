810df7faca2e692f42bd3eef6a4190c1f309cae0
unknown <trowbrid16@EN205-15825.campus.up.edu>
2013-11-18 22:53:26 -0800

    Nick poo

---
+++ DevonNick/src/com/cs301/cribbage/CbgHumanPlayer.java
@@ -0,0 +1,33 @@
+package com.cs301.cribbage;
+/* 
+   
+ */
+
+
+class CbgHumanPlayer {
+
+
+
+    /*
+     * Override
+     */
+
+    public final void sendCardToCrib() {
+    
+    }
+
+    public final void confirm() {
+    
+    }
+
+
+    /*
+     * Override
+     */
+
+    public final void sendCardToTable() {
+    
+    }
+
+
+}
---
+++ DevonNick/src/com/cs301/cribbage/CbgPlayer.java
@@ -0,0 +1,21 @@
+package com.cs301.cribbage;
+import edu.up.cs301.card.*;
+
+/* 
+   
+ */
+
+
+abstract class CbgPlayer {
+
+
+    public final void  sendCardToCrib(Card card) {
+    
+    }
+
+    public final void sendCardToTable(Card card) {
+    	
+    }
+
+
+}
