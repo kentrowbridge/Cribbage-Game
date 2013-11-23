4b8e4d4e063791a325375577c1fc5a55113e3175
trowbrid16 <trowbrid16@up.edu>
2013-11-23 15:06:45 -0800

    HumanPlayer Updates

--- Cribbage/src/com/cs301/cribbage/CbgHumanPlayer.java
+++ Cribbage/src/com/cs301/cribbage/CbgHumanPlayer.java
@@ -28,8 +28,8 @@ class CbgHumanPlayer extends GameHumanPlayer implements OnClickListener, Animato
 	private CbgState state;//state reference variable
 	private GameMainActivity myActivity;//activity reference variable
 
-	private SurfaceView mainAS;
-	
+	private AnimationSurface mainAS;
+
 	private TextView tallyCount;
 	private TextView tutorial;
 	private TextView p2Score;
@@ -45,12 +45,11 @@ class CbgHumanPlayer extends GameHumanPlayer implements OnClickListener, Animato
 	private RectF[] tableCardPos = new RectF[10];//positions where table cards are drawn
 	private Card[] tempHand;//temporary hand for changing
 	private RectF[] throwPos = new RectF[4];//position of cards in crib
-	private Card[] toThrow = new Card[2];//array that holds cards that have been selected to throw
-	private Card toTable;//card that user has selected to send to table
+	private Card [] selectedCards = new Card[2];
 	private Card[] crib;//crib reference array
 	private ArrayList<Card> cardsOnTable = new ArrayList<Card>();//arraylist of cards currently on the table
 	private GameAction action;//action that will be sent to the game
-	
+
 	public CbgHumanPlayer(String name) {
 		super(name);
 		handCardPos[0] = new RectF(0, 0, 0, 0);
@@ -63,20 +62,23 @@ class CbgHumanPlayer extends GameHumanPlayer implements OnClickListener, Animato
 
 	public final void confirm() { 
 		action = null;//resets action		
-		if(state.getGameStage() == state.THROW_STAGE){//checks if game is in throw stage
-			action = new CardsToThrow(this, toThrow);//sets action
-			for(int i = 0; i < 2;i++){
-				int cardPos = indexOfCard(tempHand, toThrow[i]);
-				if (cardPos >0 && cardPos < tempHand.length-1){
-					//if card is in hand, remove that card
+		if(state.getGameStage() == state.THROW_STAGE){//checks if game is in throw stage			
+			for(int i = 0; i < selectedCards.length;i++){
+				int cardPos = indexOfCard(tempHand, selectedCards[i]);
+				if (cardPos >0 && cardPos < tempHand.length){
+					//removes cards
 					tempHand[cardPos] = null;
 				}
 			}
+			action = new CardsToThrow(this, selectedCards);//sets action
 		}else if(state.getGameStage() == state.PEG_STAGE){//checks if game is in peg stage
-			action = new CardsToTable(this, toTable);//sets action
-			tempHand[indexOfCard(tempHand, toTable)] = null;//gets index of card played and removes the card
+			action = new CardsToTable(this, selectedCards[0]);//sets action
+			tempHand[indexOfCard(tempHand, selectedCards[0])] = null;//gets index of card played and removes the card
 		}
-		game.sendAction(action);
+		selectedCards[0] = null;
+		selectedCards[1] = null;
+			
+		game.sendAction(action);//sends game action
 		//sets tempHand to players hand
 		state.setHand(tempHand, state.PLAYER_1);
 	}
@@ -89,8 +91,6 @@ class CbgHumanPlayer extends GameHumanPlayer implements OnClickListener, Animato
 		p2Score.setText(""+state.player2Score);
 
 		tutorial.setText(state.tutorialTexts[state.gameStage]);
-
-		//TODO do we need to add players hand in here?
 	}
 
 	@Override
@@ -99,7 +99,8 @@ class CbgHumanPlayer extends GameHumanPlayer implements OnClickListener, Animato
 		myActivity.setContentView(R.layout.activity_main);
 
 		mainAS = (AnimationSurface) myActivity.findViewById(R.id.mainAS);
-		
+		mainAS.setAnimator(this);
+
 		tallyCount = (TextView) myActivity.findViewById(R.id.tallyCount);
 		tutorial = (TextView) myActivity.findViewById(R.id.tutorialText);
 		p2Score = (TextView) myActivity.findViewById(R.id.p2Score);
@@ -149,12 +150,13 @@ class CbgHumanPlayer extends GameHumanPlayer implements OnClickListener, Animato
 		float cardWidth = c.getWidth()/9;
 		float height = c.getHeight();
 		//init rectangles for cards to be drawn on
-		handCardPos[0] = new RectF(2*cardWidth, height*2/3 , 3*cardWidth, height);
-		handCardPos[1] = new RectF(3*cardWidth, height*2/3 , 4*cardWidth ,height);
-		handCardPos[2] = new RectF(4*cardWidth, height*2/3 , 5*cardWidth, height);
-		handCardPos[3] = new RectF(5*cardWidth, height*2/3 , 6*cardWidth, height);
-		handCardPos[4] = new RectF(6*cardWidth, height*2/3 , 7*cardWidth, height);
-		handCardPos[5] = new RectF(7*cardWidth, height*2/3 , 8*cardWidth, height);
+		handCardPos[0] = new RectF(  cardWidth, height*2/3 , 2*cardWidth, height);
+		handCardPos[1] = new RectF(2*cardWidth, height*2/3 , 3*cardWidth ,height);
+		handCardPos[2] = new RectF(3*cardWidth, height*2/3 , 4*cardWidth, height);
+		handCardPos[3] = new RectF(4*cardWidth, height*2/3 , 5*cardWidth, height);
+		handCardPos[4] = new RectF(5*cardWidth, height*2/3 , 6*cardWidth, height);
+		handCardPos[5] = new RectF(6*cardWidth, height*2/3 , 7*cardWidth, height);
+
 		tableCardPos[0] = new RectF(  cardWidth, 0,   cardWidth, height/3);
 		tableCardPos[1] = new RectF(2*cardWidth, 0, 2*cardWidth, height/3);
 		tableCardPos[2] = new RectF(3*cardWidth, 0, 3*cardWidth, height/3);
@@ -165,78 +167,89 @@ class CbgHumanPlayer extends GameHumanPlayer implements OnClickListener, Animato
 		tableCardPos[7] = new RectF(8*cardWidth, 0, 8*cardWidth, height/3);
 		tableCardPos[8] = new RectF(9*cardWidth, 0, 9*cardWidth, height/3);
 		tableCardPos[9] = new RectF(9*cardWidth, 0, 10*cardWidth, height/3);
-		throwPos[0] = new RectF(9*cardWidth, height*2/3, 10*cardWidth, height);
-		throwPos[1] = new RectF(9*cardWidth + cardWidth/3, height*2/3, 11*cardWidth, height);
+
+		throwPos[0] = new RectF(9*cardWidth                , height*2/3, 10*cardWidth, height);
+		throwPos[1] = new RectF(9*cardWidth + cardWidth/3  , height*2/3, 11*cardWidth, height);
 		throwPos[2] = new RectF(9*cardWidth + cardWidth*2/3, height*2/3, 10*cardWidth, height);
-		throwPos[3] = new RectF(10*cardWidth, height*2/3, 11*cardWidth, height);
+		throwPos[3] = new RectF(10*cardWidth               , height*2/3, 11*cardWidth, height);
 
 		//inits local variables from state variables (NEED THIS)
 		tempHand = state.getHand();
 		cardsOnTable = state.getTable();
 		crib = state.getCrib();
-		if(tempHand != null){
-			for(int i = 0; i < tempHand.length;i++){
+		for(int i = 0; i < tempHand.length;i++){
+			if(tempHand[i] != null){//if card is null, dont draw it
 				tempHand[i].drawOn(c, handCardPos[i]);//draws the cards in each position
+			}else if(tempHand[i] == null){
+				c.drawRect(handCardPos[i], new Paint(Color.BLACK));
 			}
 		}
-		boolean cribNull = false;
-		for (Card cards : crib){
-			if (cards == null){
-				cribNull = true;
-			}
-		}
-		if(!cribNull){
+
+
+		if(isFull(crib)){
 			for(int i = 0; i < crib.length;i++){
 				crib[i].drawOn(c,throwPos[i]);//draws the cards in each position
 			}
 		}
-
-		if(cardsOnTable != null){
-			int counter = 0;
-
-			for(Card cOnT : cardsOnTable){
-				cOnT.drawOn(c, tableCardPos[counter]);
-				counter++;
-			}
-
+		if(selectedCards[0] != null){
+			highLight(c, handCardPos[indexOfCard(tempHand, selectedCards[0])]);
+		}
+		if(selectedCards[1] !=null){
+			highLight(c, handCardPos[indexOfCard(tempHand,selectedCards[1])]);
 		}
 	}
-	
+
 	@Override
 	public void onClick(View v) {
 		if(v.getId() == confirm.getId()){
-			if(state.getGameStage() == CbgState.THROW_STAGE){
-				game.sendAction(new CardsToThrow(this, toThrow));
-			}
-			if(state.getGameStage() == CbgState.PEG_STAGE){
-				game.sendAction(new CardsToTable(this, toTable));
-			}
+			confirm();
 		}else if (v.getId() == R.id.quitButton){
 			System.exit(0);
 		}
 	}
 
-	private void selectCard(Card c) {
-		if(state.getGameStage() == CbgState.THROW_STAGE){
-			if(toThrow[0] == null && toThrow[1] == null){//check if toThrow is empty
-				toThrow[0] = c;
-			}else if(toThrow[0] !=null && toThrow[1] == null){//check if toThrow has only one entry
-				toThrow[1] =c;
-			}else{//all other situations do nothing
+	private void selectCard(Card c) {		
+		if(state.getGameStage() == state.THROW_STAGE){
+			if(selectedCards[0] == c){
+				selectedCards[0] = null;//selects
+			}else if(selectedCards[1] == c){
+				selectedCards[1] = null;//selects
+			}else if(selectedCards[0] == null){
+				selectedCards[0] = c;//deselects
+			}else if(selectedCards[1] == null){
+				selectedCards[1] = c;//deselects
 			}
-		}
-		else if(state.getGameStage() == CbgState.PEG_STAGE){
-			toTable = c;
-		}
+		}else if(state.getGameStage() == state.PEG_STAGE){
+			if(selectedCards[0] == null){
+				selectedCards[0] = c;//selects
+			}else if(selectedCards[0] == c){
+				selectedCards[0] = null;//deselects
+			}
+		}		
 	}
 
-	public int indexOfCard(Card[] a, Card c){
+	private int indexOfCard(Card[] a, Card c){
 		for(int i =0; i<a.length;i++){
 			if(a[i]==c){
 				return i;
 			}
 		}
 		return -1;
+	}
+
+	private void highLight(Canvas c, RectF where){
+		if(where != null){
+			Paint highlight = new Paint(Color.BLACK);
+			highlight.setAlpha(60);
+			c.drawRect(where, highlight);
+		}
+	}
+
+	private boolean isFull(Card[] arr){
+		for(Card c: arr){
+			if(c==null){return false;}
+		}
+		return true;
 	}
 
 	@Override
@@ -256,26 +269,25 @@ class CbgHumanPlayer extends GameHumanPlayer implements OnClickListener, Animato
 
 	@Override
 	public void onTouch(MotionEvent event) {
-		float x = event.getX();
-		float y = event.getY();	
+		if(event.getAction() == event.ACTION_DOWN){
+			float x = event.getX();
+			float y = event.getY();	
 
-		if(state != null && state.getGameStage() == CbgState.THROW_STAGE && state.getTurn() == state.PLAYER_1){//checks if state is not null, cecks stage
-			//and checks whose turn it is
-			for(int i = 0; i<6; i++){
-				if(handCardPos[i] != null && handCardPos[i].contains(x, y)){
-					selectCard(tempHand[i]);
+			if(state != null && state.getGameStage() == CbgState.THROW_STAGE && state.getTurn() == state.PLAYER_1){//checks if state is not null, cecks stage
+				//and checks whose turn it is
+				for(int i = 0; i<tempHand.length; i++){
+					if(handCardPos[i] != null && handCardPos[i].contains(x, y)){
+						selectCard(tempHand[i]);//selects card in 
+					}
 				}
 			}
-		}
-		if(state != null && state.getGameStage() == CbgState.PEG_STAGE && state.getTurn() == state.PLAYER_1){
-			for(int i = 0; i<6;i++){
-				if(handCardPos[i] != null && handCardPos[i].contains(x, y)){
-					selectCard(tempHand[i]);
+			if(state != null && state.getGameStage() == CbgState.PEG_STAGE && state.getTurn() == state.PLAYER_1){
+				for(int i = 0; i<tempHand.length;i++){
+					if(handCardPos[i] != null && handCardPos[i].contains(x, y)){
+						selectCard(tempHand[i]);
+					}
 				}
 			}
-		}
+		}	
 	}
-	
-	//TODO make a get playerIDX method
-	
 }
